d206b591159dc42f0ccc7e4e83ccb5109392d998
usncahill <usncahill@hotmail.com>
2023-09-19 23:24:08 -0400

    Update node_helper.js
    Support multi-user auth implementation

--- node_helper.js
+++ node_helper.js
@@ -8,77 +8,79 @@ module.exports = NodeHelper.create({
         
         console.log("Starting node helper for: " + this.name);
 
-        this.oAuth2Client;
-        this.service;
+        this.oAuth2Client = {};
+        this.service = {};
+        this.config = {};
     },
 
     socketNotificationReceived: function(notification, payload) {
 
+        this.config[payload.listID] = payload; // save config stuff for later as needed
         if (notification === "MODULE_READY") {
-            if(!this.service) {
-                this.authenticate();
+            if(!this.service[payload.listID]) {
+                this.authenticate(payload.listID);
             } else {
                 // Check if tasks service is already running, avoids running authentication twice
-                console.log("TASKS SERVICE ALREADY RUNNING, DONT NEED TO AUTHENTICATE AGAIN")
+                console.log("Task service for list: " + payload.listID + " already running.");
                 this.sendSocketNotification("SERVICE_READY", {});
             }
         } else if (notification === "REQUEST_UPDATE") {
-            this.getList(payload);
-	}
+            this.getList(payload.listID);
+        }
     },
 
-    authenticate: function() {
+    authenticate: function(listID) {
         var self = this;
 
-        fs.readFile(self.path + '/credentials.json', (err, content) => {
+        fs.readFile(self.path + '/credentials' + self.config[listID].listName + '.json', (err, content) => {
             if (err) {
-		var payload = {code: err.code, message: err.message, details: err.details};
+                var payload = {code: err.code, message: err.message, details: err.details};
 
-		self.sendSocketNotification("TASKS_API_ERROR", payload);
-		return console.log('Error loading client secret file:', err);
-	    }
+                self.sendSocketNotification("TASKS_API_ERROR", payload);
+                return console.log('Error loading client secret file:', err);
+            }
             // Authorize a client with credentials, then call the Google Tasks API.
-            authorize(JSON.parse(content), self.startTasksService);
+            authorize(JSON.parse(content), listID, self.startTasksService);
           });
 
-        function authorize(credentials, callback) {
+        function authorize(credentials, listID, callback) {
             const {client_secret, client_id, redirect_uris} = credentials.installed;
-            self.oAuth2Client = new google.auth.OAuth2(
+            self.oAuth2Client[listID] = new google.auth.OAuth2(
                 client_id, client_secret, redirect_uris[0]);
           
             // Check if we have previously stored a token.
-            fs.readFile(self.path + '/token.json', (err, token) => {
-              if (err) return console.log('Error loading token');
-              self.oAuth2Client.setCredentials(JSON.parse(token));
-              callback(self.oAuth2Client, self);
+            fs.readFile(self.path + '/token' + self.config[listID].listName + '.json', (err, token) => {
+                if (err) return console.log('Error loading token');
+                self.oAuth2Client[listID].setCredentials(JSON.parse(token));
+                callback(self.oAuth2Client[listID], self, listID);
             });
         }
     },
 
-    startTasksService: function(auth, self) {
-        self.service = google.tasks({version: 'v1', auth});
+    startTasksService: function(auth, self, listID) {
+        self.service[listID] = google.tasks({version: 'v1', auth});
         self.sendSocketNotification("SERVICE_READY", {});
     },
 
-    getList: function(config) {
+    getList: function(listID) {
         var self = this;
 
-        if(!self.service) {
+        if(!self.service[listID]) {
             console.log("Refresh required"); 
             return;
         }
 
-        self.service.tasks.list({
-            tasklist: config.listID,
-            maxResults: config.maxResults,
-            showCompleted: config.showCompleted,
-            showHidden: config.showHidden,
+        self.service[listID].tasks.list({
+            tasklist: listID,
+            maxResults: self.config[listID].maxResults,
+            showCompleted: self.config[listID].showCompleted,
+            showHidden: self.config[listID].showHidden,
         }, (err, res) => {
             if (err) {
-		var payload = {code: err.code, message: err.message, details: err.details};
-		self.sendSocketNotification("TASKS_API_ERROR", payload);
-	 	return console.error('The API returned an error: ' + err);
-	    }
+                var payload = {code: err.code, message: err.message, details: err.details};
+                self.sendSocketNotification("TASKS_API_ERROR", payload);
+                return console.error('The API returned an error: ' + err);
+            }
 
             // Testing
             /* 
@@ -93,7 +95,7 @@ module.exports = NodeHelper.create({
             }
              */
 
-            var payload = {id: config.listID, items: res.data.items};
+            var payload = {id: listID, items: res.data.items};
             self.sendSocketNotification("UPDATE_DATA", payload);
         });
     },
